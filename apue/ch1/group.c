c29b9d879609175219be9d9093217331053b7de4
郑天i <774826724@qq.com>
2017-09-20 16:55:57 +0800

    add

---
+++ apue/ch1/group.c
@@ -0,0 +1,5 @@
+#include "../apue.h"
+
+int main() {
+    printf("groupid:%u, userid:%u\n", getgid(), getuid());
+}
--- apue/ch1/shell.c
+++ apue/ch1/shell.c
@@ -1,10 +1,15 @@
 #include "../apue.h"
 #include <sys/wait.h>
 
+static void sig_int(int);
+
 int main() {
     char buf[MAXLINE];
     pid_t pid;
     int status;
+
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+        err_sys("signal error");
 
     printf("%%");
     while(fgets(buf, MAXLINE, stdin) != NULL) {
@@ -24,4 +29,8 @@ int main() {
         printf("%%");
     }
     exit(0);
+}
+
+void sig_int(int signo) {
+    printf("interrupt\n%%");
 }
