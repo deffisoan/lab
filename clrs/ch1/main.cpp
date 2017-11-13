a1e4d20ee30ff84be207d7c0301a401fc0abbd74
deffisoan <deffisoan@gmail.com>
2017-11-13 15:18:30 +0800

    update

--- clrs/ch1/main.cpp
+++ clrs/ch1/main.cpp
@@ -1,10 +1,11 @@
 #include "insert_sort.h"
 
 int main() {
-    std::vector<int> m;
-    m.insert(m.end(), 1);
-    m.insert(m.end(), 4);
-    m.insert(m.end(), 3);
+    std::vector<int> m = {1, 4, 3};
+
+    // m.insert(m.end(), 1);
+    // m.insert(m.end(), 4);
+    // m.insert(m.end(), 3);
     Sort<int> sort;
     sort.insert_sort(m);
 }
